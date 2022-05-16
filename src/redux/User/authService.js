dfd97aeefdc69e24738d1bf614c47711d16a0640
Abu David <davidabu80@gmail.com>
2022-05-16 15:28:01 +0100

    Error Fix

--- src/redux/User/authService.js
+++ src/redux/User/authService.js
@@ -3,10 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 //Base URL
 const API_URL = process.env.APP_BASE_URL;
-
 //Register User
 const register = async (userData) => {
+  console.log(API_URL);
   const response = await axios.post(`${API_URL}/buyer/register`, userData);
+
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data.token));
   }
