7191d63045a8dd515bd05f588b9d6820fb83eaa1
Abu David <davidabu80@gmail.com>
2022-05-16 13:25:25 +0100

    Login

--- next.config.js
+++ next.config.js
@@ -7,4 +7,10 @@ module.exports = {
   env: {
     APP_BASE_URL:  'https://atoovis-server.herokuapp.com/v1'
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 }
