b34d7e08dc8c0a9b09014476edfbbeda0abc417a
Abu David <davidabu80@gmail.com>
2022-05-24 11:51:56 +0100

    Res

--- src/components/User/FeaturedProduct.tsx
+++ src/components/User/FeaturedProduct.tsx
@@ -17,29 +17,31 @@ const FeaturedProduct = () => {
         <p className='text-color'>See All</p>
       </div>
       <div className='ft'>
-        <Row gutter={25} justify='space-around'>
+        <Row gutter={2} justify='space-around'>
           {RawProuducts.map(({ id, info, price }) => (
-            <Col key={id} xl={6} lg={6}>
-              <div className='ft'>
-                <img src='/imh.png' className='.img' alt='Home' />
-                <p>{info}</p>
-                <div>
+            <Row gutter={12} >
+              <Col key={id} xl={6} lg={6} sm={12} xs={6}>
+                <div className='ft'>
+                  <img src='/imh.png' className='.img' alt='Home' />
+                  <p>{info}</p>
                   <div>
-                    <p>{price}</p>
                     <div>
-                      <BeautyStars
-                        size='12px'
-                        gap='7px'
-                        activeColor='#ffaf38'
-                        inactiveColor='#ddd'
-                        value={5}
-                      />
+                      <p>{price}</p>
+                      <div>
+                        <BeautyStars
+                          size='12px'
+                          gap='7px'
+                          activeColor='#ffaf38'
+                          inactiveColor='#ddd'
+                          value={5}
+                        />
+                      </div>
                     </div>
+                    <Button className='but-ton'>Add To Cart</Button>
                   </div>
-                  <Button className='but-ton'>Add To Cart</Button>
                 </div>
-              </div>
-            </Col>
+              </Col>
+            </Row>
           ))}
         </Row>
       </div>
