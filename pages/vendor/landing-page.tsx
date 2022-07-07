aadce6d2cadb6851c82102598c235cf93cf128c6
Abu David <davidabu80@gmail.com>
2022-07-07 15:35:44 +0100

    Vendor Updates

---
+++ pages/vendor/landing-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImgLanding from '../../src/components/Vendor/ImgLanding'
+
+const LandingPageVendor = () => {
+  return (
+    <div>
+        <ImgLanding/>
+    </div>
+  )
+}
+
+export default LandingPageVendor
